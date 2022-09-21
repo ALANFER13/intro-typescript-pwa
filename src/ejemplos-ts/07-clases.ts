ea60643f8d79d5419664508f4b501e2047577a83
ALANFER13 <alanfersp13@gmail.com>
2022-09-21 14:53:45 -0500

    primer commit

---
+++ src/ejemplos-ts/07-clases.ts
@@ -0,0 +1,13 @@
+
+//Clases
+
+class Persona{
+    nombre:string;
+    edad:number;
+
+
+    imprimir():void{
+        console.log(`El nombre es: ${this.nombre} \n
+        La edad es: ${this.edad}`);
+    }
+}
